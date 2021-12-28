fcff65ebd3c85da331d3fa1ce7651b7188fa465d
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-28 22:50:06 +0300

    Arrays and Sets

---
+++ Day2/Day2.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+
+/// Arrays
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+beatles[1]
+
+/// Sets
+let colors = Set(["red", "green", "blue"])
+let colors2 = Set(["red", "green", "blue", "red"])
