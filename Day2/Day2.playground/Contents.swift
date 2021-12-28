1d5b4d68c6a465d5ac196382eb656239f2159f07
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-28 22:54:04 +0300

    Tuples

--- Day2/Day2.playground/Contents.swift
+++ Day2/Day2.playground/Contents.swift
@@ -14,3 +14,11 @@ beatles[1]
 /// Sets
 let colors = Set(["red", "green", "blue"])
 let colors2 = Set(["red", "green", "blue", "red"])
+
+
+/// Tuples
+var name = (first: "Taylor", last: "Swift")
+name.1
+name.first
+name.first = "Taylor New"
+// Error: name.first = 35
