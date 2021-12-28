3cf7b8f4c4921e10b932b88ed31be2c6a0fc1cac
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-28 14:27:44 +0300

    String and Integer

---
+++ Day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Darwin
+
+var greeting = "Hello, playground"
+
+greeting = "Changed"
+ 
+
+/// Strings and Integers
+var age = 38
+var population = 8_000_000
+
+/// Multi-line strings
+
+
