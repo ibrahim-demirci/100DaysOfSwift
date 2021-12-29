465a7838585d2e646c78e1ffd38f3671338fca99
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-29 21:40:41 +0300

    Arithmetic Operators

---
+++ Day3/Day3.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+/// Arithmetic Operators
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remaind = 13 % firstScore
+
+
