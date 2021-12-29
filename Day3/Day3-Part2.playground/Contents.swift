8399c89ad6807787e0b8b117eea71a12ff149bb2
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-29 23:32:55 +0300

    Combining Operators

--- Day3/Day3-Part2.playground/Contents.swift
+++ Day3/Day3-Part2.playground/Contents.swift
@@ -14,4 +14,15 @@ if firstCard + secondCard == 2 {
     print("Regular cards")
 }
 
+/// Combining Operators
 
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both are over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("One of them is over 18")
+}
