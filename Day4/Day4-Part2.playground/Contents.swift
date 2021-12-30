8e4dee295bf63282aca294c805df472446ab72ac
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2021-12-30 13:12:26 +0300

    Exiting loops

---
+++ Day4/Day4-Part2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+/// Exiting Loops
+var countDown = 10
+
+while countDown >= 0 {
+    print(countDown)
+    
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+    countDown -= 1
+}
