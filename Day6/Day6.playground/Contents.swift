39286a1a955450ec0a2600e1bf67014970cffdb1
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-14 00:16:24 +0300

    Closures As Parameters

--- Day6/Day6.playground/Contents.swift
+++ Day6/Day6.playground/Contents.swift
@@ -19,3 +19,12 @@ let drivingWithReturn = { (place: String) -> String in
     return "I'm goint to \(place) in my car"
 }
 let message = drivingWithReturn("Berlin")
+
+
+/// Closures As Parameters
+func travel(action: () -> Void) {
+    print("I'm ready to go")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
