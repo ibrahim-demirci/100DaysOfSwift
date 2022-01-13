638ce18eed3e6f47346ea6fad8e1190ddacde134
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-14 00:08:10 +0300

    Returning Values

--- Day6/Day6.playground/Contents.swift
+++ Day6/Day6.playground/Contents.swift
@@ -12,3 +12,10 @@ let drivingParameter = { (place: String) in
     print("I'm goint to \(place) in my car")
 }
 drivingParameter("London")
+
+
+/// Returning Values
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm goint to \(place) in my car"
+}
+let message = drivingWithReturn("Berlin")
