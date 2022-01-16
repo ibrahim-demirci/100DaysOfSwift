a225e1a408878e8a7bfd71b1d6fe5b166e821083
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-16 21:46:03 +0300

    Properties and Methods of Arrays

--- Day8/Day8-Part2.playground/Contents.swift
+++ Day8/Day8-Part2.playground/Contents.swift
@@ -7,3 +7,13 @@ print(string.count)
 print(string.hasPrefix("Do"))
 print(string.uppercased())
 print(string.sorted())
+
+
+/// Properties and Methods of Arrays
+var toys = ["Woody"]
+
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
