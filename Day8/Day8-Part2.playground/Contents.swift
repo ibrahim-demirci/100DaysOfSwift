86a14d4d813e359cd29a299e58e5fd0ddda46dca
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-16 21:40:54 +0300

    Properties and Methods of Strings

---
+++ Day8/Day8-Part2.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+/// Properties and Methods of Strings
+let string = "Do or do not, there is no try."
+
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
