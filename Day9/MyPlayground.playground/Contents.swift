45456c57905501ad5c15641a5958e037a537d415
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-17 23:29:44 +0300

    Lazy Properties

--- Day9/MyPlayground.playground/Contents.swift
+++ Day9/MyPlayground.playground/Contents.swift
@@ -14,5 +14,30 @@ var user = User()
 user.username = "twostraws"
 
 
-/// 
+/// Referring to the Current Instance
+struct Person {
+    var name: String
 
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+
+/// Lazy Properties
+struct FamilyTree {
+init() {
+    print("Creating family tree!")
+}
+}
+struct Member {
+    var name: String
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+var ed = Member(name: "Ed")
+ed.familyTree
