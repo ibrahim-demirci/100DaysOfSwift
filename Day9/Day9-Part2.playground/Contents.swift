94bc6a1fe7a914422a6f0a631eaba2d6f90694dd
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-18 00:23:17 +0300

    Access Control

---
+++ Day9/Day9-Part2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+/// Access Control
+struct Person {
+    private var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+let ed = Person(id: "12345")
+ed.identify()
+
