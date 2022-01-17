4cc0f1f1887f105b2486d76e4e9f54f26d9e466a
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-17 23:14:44 +0300

    Initializers

---
+++ Day9/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+
+/// Initializers
+struct User {
+    var username: String
+
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+var user = User()
+user.username = "twostraws"
+
+
+/// 
+
