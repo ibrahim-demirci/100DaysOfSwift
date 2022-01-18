c26a4e669ce2af1bc71641263605ef78994fd047
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-18 23:22:11 +0300

    Deinitiializers

--- Day10/Day10-Part2.playground/Contents.swift
+++ Day10/Day10-Part2.playground/Contents.swift
@@ -12,3 +12,21 @@ var singerCopy = singer
 singerCopy.name = "Azer Bulbul"
 
 print(singer.name)
+
+
+/// Deinitiializers
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive!")
+    }
+
+    deinit {
+        print("\(name) is no more!")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+}
