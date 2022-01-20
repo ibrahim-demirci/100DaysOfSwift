e825fa88f2a0d0c851664ae39aa4b1b49275417c
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-21 01:29:11 +0300

    Unwrapping Optionals

--- Day12/MyPlayground.playground/Contents.swift
+++ Day12/MyPlayground.playground/Contents.swift
@@ -4,3 +4,12 @@ import UIKit
 /// Handling Missing Data
 var age: Int? = nil
 age = 38
+
+
+///  Unwrapping Optionals
+var name: String? = nil
+if let unwrapped = name {
+    print("\(unwrapped.count) letters")
+} else {
+    print("Missing name.")
+}
