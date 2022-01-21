190b5af60a98a98235aff6ec3e71d242330fab02
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-21 22:10:33 +0300

    Failable  Initializers

--- Day12/MyPlayground.playground/Contents.swift
+++ Day12/MyPlayground.playground/Contents.swift
@@ -68,3 +68,18 @@ if let result = try? checkPassword("password") {
 } else {
     print("D'oh.")
 }
+
+
+/// Failable  Initializers
+struct Person {
+    var id: String
+
+    init?(id: String) {
+        if id.count == 9 {
+            self.id = id
+        } else {
+            return nil
+        }
+    }
+}
+
