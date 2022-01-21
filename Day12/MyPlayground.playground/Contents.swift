02ba1e00361666777d08f515b31903040655324f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-21 22:17:43 +0300

    Typecasting

--- Day12/MyPlayground.playground/Contents.swift
+++ Day12/MyPlayground.playground/Contents.swift
@@ -83,3 +83,18 @@ struct Person {
     }
 }
 
+/// Typecasting
+class Animal { }
+class Fish: Animal { }
+
+class Dog: Animal {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+let pets = [Fish(), Dog(), Fish(), Dog()]
+for pet in pets {
+    if let dog = pet as? Dog {
+        dog.makeNoise()
+    }
+}
