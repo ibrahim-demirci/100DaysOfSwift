b142e9316148ce184d7318c09ec8e35117ad191b
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-01-27 22:43:56 +0300

    Add onTapped function and alert controller

--- Day19-20-21/Project2/Project2/ViewController.swift
+++ Day19-20-21/Project2/Project2/ViewController.swift
@@ -28,20 +28,34 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco",
                       "nigeria", "poland", "russia", "spain", "uk", "us"]
-        askQuestion()
-        title = countries[correctAnswer].uppercased()
-        
+        askQuestion(action: nil)
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction!) {
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+
     }
     
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
 
 }
 
