17fd40edb83abc88810bc39b5138c0cbc2899600
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-09 00:43:17 +0300

    Challenge 3: Add 1.5 mainView height for each label

--- Day30-31/Project6b/Project6b/ViewController.swift
+++ Day30-31/Project6b/Project6b/ViewController.swift
@@ -63,7 +63,7 @@ class ViewController: UIViewController {
            // label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
             label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
 
             if let previous = previous {
                 // we have a previous label – create a height constraint
