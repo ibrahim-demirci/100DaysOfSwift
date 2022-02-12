66a81fde8fd8faa7c782b43fb1ba8b16a24c2688
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-12 17:24:47 +0300

    Create models and get data from internet

---
+++ Day33-34-35/Project7/Project7/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petı.swift
+//  Project7
+//
+//  Created by İbrahim Demirci on 12.02.2022.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
+
