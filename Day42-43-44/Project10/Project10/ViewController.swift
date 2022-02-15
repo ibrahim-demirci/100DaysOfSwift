ebadf56b2f43dc53275a887387a954b15758999f
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-15 18:07:35 +0300

    Create Projet10

---
+++ Day42-43-44/Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by İbrahim Demirci on 15.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
