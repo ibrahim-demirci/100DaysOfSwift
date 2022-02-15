72ca89e4ac3fe9ffff867ff66b3672f175f13754
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-15 18:27:53 +0300

    Create PersonCell

---
+++ Day42-43-44/Project10/Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by İbrahim Demirci on 15.02.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
