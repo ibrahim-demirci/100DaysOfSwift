4b0948eac2e93cfb849015e1e28b9d824e7e52dc
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-15 22:21:34 +0300

    Create Person class

---
+++ Day42-43-44/Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by İbrahim Demirci on 15.02.2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    
+    
+}
