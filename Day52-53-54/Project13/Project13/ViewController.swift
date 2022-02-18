f246bb9cec775a0e606d78697f6dc1930f304383
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-18 21:36:21 +0300

    Desing storyboard

---
+++ Day52-53-54/Project13/Project13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by İbrahim Demirci on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
