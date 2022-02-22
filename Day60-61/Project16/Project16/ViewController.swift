9833a99c12f1866ad13e79f0fd0b9eff709b508c
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-22 22:19:00 +0300

    Change pinTintColor

--- Day60-61/Project16/Project16/ViewController.swift
+++ Day60-61/Project16/Project16/ViewController.swift
@@ -11,21 +11,22 @@ import MapKit
 class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet var mapView: MKMapView!
-    
-    override func viewDidLoad() {        super.viewDidLoad()
-        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
         mapView.delegate = self
-        
+
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
         let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
-        
+
         mapView.addAnnotations([london, oslo, paris, rome, washington])
 
     }
-    
+
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         // 1
         guard annotation is Capital else { return nil }
@@ -34,7 +35,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let identifier = "Capital"
 
         // 3
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
 
         if annotationView == nil {
             //4
@@ -48,6 +50,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
             // 6
             annotationView?.annotation = annotation
         }
+        annotationView?.pinTintColor = .darkGray
 
         return annotationView
     }
