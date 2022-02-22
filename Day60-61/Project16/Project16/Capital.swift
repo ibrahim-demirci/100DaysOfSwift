a1ab153d14569d2bf02deaec3d27659680318434
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-22 20:04:06 +0300

    Create Capital class and add some annotations to mapc

---
+++ Day60-61/Project16/Project16/Capital.swift
@@ -0,0 +1,25 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by İbrahim Demirci on 22.02.2022.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+    
+    
+    
+}
