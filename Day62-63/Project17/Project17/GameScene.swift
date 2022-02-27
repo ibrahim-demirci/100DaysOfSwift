370eb7b79bed773419632f46a1766d7ea08acdb4
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-27 21:44:18 +0300

    Setting up Project17

---
+++ Day62-63/Project17/Project17/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by İbrahim Demirci on 27.02.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
