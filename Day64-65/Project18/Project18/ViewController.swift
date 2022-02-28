6012fb5d47fcb56882c72013d8cab76f98c041a7
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-02-28 14:32:00 +0300

    Setting Up Project18

---
+++ Day64-65/Project18/Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by İbrahim Demirci on 28.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
