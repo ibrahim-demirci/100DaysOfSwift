1690d46848a1e44849a546e1f2226d7454a635bf
İbrahim Demirci <ibrahimdemirc.dev@gmail.com>
2022-03-02 10:53:12 +0300

    Setting up Project19

---
+++ Day67-68-69/Project19/Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by İbrahim Demirci on 2.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
